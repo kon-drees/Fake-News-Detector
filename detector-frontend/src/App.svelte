9d59f1ca3e82f046ba1adcf64361e51542463a3d
windeck99 <tobiaswindeck@outlook.de>
2025-11-12 18:11:27 +0100

    Init frontend commit

---
+++ detector-frontend/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+</script>
+
+<main>
+  <h1 class="title-text">Fake-News-Detector</h1>
+</main>
+
+<style>
+</style>
