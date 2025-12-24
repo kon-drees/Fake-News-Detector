badf7c80eb058d29daee0dd8f5d8aa9e2e15fc9b
lennart <lennartbenninghaus@googlemail.com>
2025-12-24 15:53:37 +0100

    add test for /fact-check

--- detector-backend/app/tests/test_api.py
+++ detector-backend/app/tests/test_api.py
@@ -1,12 +1,15 @@
 from typing import List
 
 from fastapi.testclient import TestClient
+from pydantic_ai.models import Model
+from pydantic_ai.models.test import TestModel
 import pytest
 from unittest.mock import patch
 
 
 from app.main import app, model
 from app.domain import Label, PredictionResult, TokenContribution
+from app.core.fact_check_agent import FactCheckAgent
 
 
 class MockFakeNewsDetector:
@@ -14,16 +17,23 @@ class MockFakeNewsDetector:
         return PredictionResult(label=Label.FAKE, score=0.95)
 
     def highlight(self, text: str) -> List[TokenContribution]:
-        return [TokenContribution("Fake", 0.9, 0.9), TokenContribution("Article", 0.1, 0.1)]
+        return [
+            TokenContribution("Fake", 0.9, 0.9),
+            TokenContribution("Article", 0.1, 0.1),
+        ]
+
+
+class MockFactCheckAgent(FactCheckAgent):
+    def load_model(self) -> Model:
+        return TestModel()
 
 
 @pytest.fixture(scope="module")
 def client():
-    model["detector"] = MockFakeNewsDetector()
-
     with patch("app.main.FakeNewsDetector", new=MockFakeNewsDetector):
-        with TestClient(app) as c:
-            yield c
+        with patch("app.main.FactCheckAgent", new=MockFactCheckAgent):
+            with TestClient(app) as c:
+                yield c
 
     model.clear()
 
@@ -57,6 +67,19 @@ def test_highlight_endpoint_success(client):
     assert isinstance(data["highlights"], list)
     assert len(data["highlights"]) == 2
     assert data["highlights"][0]["token"] == "Fake"
+
+
+def test_fact_check_endpoint_success(client):
+    payload = {"text": "Fake Article."}
+    response = client.post("/api/fact-check", json=payload)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "fake_score" in data
+    assert "summary_analysis" in data
+    assert "checked_claims" in data
+    assert isinstance(data["checked_claims"], list)
 
 
 def test_predict_validation_error(client):
